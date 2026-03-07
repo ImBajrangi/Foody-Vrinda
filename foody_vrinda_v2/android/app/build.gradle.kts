593fc63f1d61748e22d33dad89549714427fdfb5
ImBajrangi <sharmagharsh1@gmail.com>
2026-03-07 21:53:13 +0530

    feat: Enable Core Library Desugaring by setting `isCoreLibraryDesugaringEnabled` to true, updating `minSdk` to 21 or noting the requirement, and adding the `desugar_jdk_libs` dependency.

--- foody_vrinda_v2/android/app/build.gradle.kts
+++ foody_vrinda_v2/android/app/build.gradle.kts
@@ -11,6 +11,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -24,8 +25,8 @@ android {
         applicationId = "com.foodyvrinda.foody_vrinda_v2"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21 // Required for coreLibraryDesugaring
+        targetSdk = flutter.targetSdk
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -37,6 +38,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
